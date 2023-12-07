27eb8d3eb8952d68d81d4c6cd3a53dbd2ecb5437
k-lila <krishnalilapalandi@gmail.com>
2023-12-07 13:47:17 -0300

    resolvido

---
+++ src/exercicio_ts.js
@@ -0,0 +1,16 @@
+"use strict";
+function multiplica(x, y) {
+    return x * y;
+}
+;
+function saudar(nome) {
+    return `Olá ${nome}`;
+}
+;
+// ----------------------------------------------+
+let num1 = 10;
+let num2 = 0.1;
+console.log(multiplica(num1, num2));
+// --------------------------------
+let nome = 'Fulano de Tal';
+console.log(saudar(nome));
---
+++ src/exercicio_ts.ts
@@ -0,0 +1,13 @@
+function multiplica(x: number, y: number): number {
+    return x * y
+};
+function saudar(nome:string): string {
+    return `Olá ${nome}`
+};
+// ----------------------------------------------+
+let num1:number = 10;
+let num2:number = 0.1;
+console.log(multiplica(num1, num2))
+// --------------------------------
+let nome:string = 'Fulano de Tal';
+console.log(saudar(nome))
